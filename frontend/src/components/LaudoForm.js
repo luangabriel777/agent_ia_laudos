4308664bc0cec71ed9ae14a6aebae9e7c0fc7fc8
LuanGabriel <71942707+luangabriel777@users.noreply.github.com>
2025-07-26 17:50:49 -0300

    Create LaudoForm.js

---
+++ frontend/src/components/LaudoForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LaudoForm() {
+  return (
+    <div>
+      {/* TODO: implementar campos do formulário de laudo */}
+      <p>Laudo form placeholder</p>
+    </div>
+  );
+}
+
+export default LaudoForm;
