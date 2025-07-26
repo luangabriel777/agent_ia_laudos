8a37f5d4c6127f0f7f6198644de5ee204d21fb9a
LuanGabriel <71942707+luangabriel777@users.noreply.github.com>
2025-07-26 17:50:51 -0300

    Create LaudoList.js

---
+++ frontend/src/components/LaudoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LaudoList() {
+  return (
+    <div>
+      {/* TODO: implementar lista de laudos */}
+      <p>Laudo list placeholder</p>
+    </div>
+  );
+}
+
+export default LaudoList;
